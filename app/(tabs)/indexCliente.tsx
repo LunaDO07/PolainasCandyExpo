ba043bd8f2cfd411672960ff50ec7610c90cb644
LitzyDiaz0 <213700011@utna.edu.mx>
2024-08-11 10:27:08 -0600

    acomodo de elementos en indexCliente

--- app/(tabs)/indexCliente.tsx
+++ app/(tabs)/indexCliente.tsx
@@ -3,6 +3,7 @@ import { View, Text, Image, StyleSheet, ScrollView, Modal, Dimensions , Touchabl
 import { Ionicons } from '@expo/vector-icons';
 import NavBusqueda from '../../components/navBusqueda';
 import { ImageSourcePropType } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -16,12 +17,12 @@ interface CategoryItem {
 const items: CategoryItem[] = [
     { id: '1', name: 'Gomitas', image: require('../../assets/images/gomitas.jpg') },
     { id: '2', name: 'Chicles', image: require('../../assets/images/chicles.jpg') },
-    { id: '3', name: 'Chocolates', image: require('../../assets/images/chocolate.jpg') },
+    { id: '3', name: 'Chocolate', image: require('../../assets/images/chocolate.jpg') },
     { id: '4', name: 'Paletas', image: require('../../assets/images/paletas.jpg') },
     { id: '5', name: 'Galletas', image: require('../../assets/images/gall2.jpg') },
     { id: '6', name: 'Botana', image: require('../../assets/images/botana2.jpg') },
-    { id: '7', name: 'Bombones', image: require('../../assets/images/bombones.jpg') },
-    { id: '8', name: 'Tamarindos', image: require('../../assets/images/tamarindo.jpg') },
+    { id: '7', name: 'Bombon', image: require('../../assets/images/bombones.jpg') },
+    { id: '8', name: 'Tamarindo', image: require('../../assets/images/tamarindo.jpg') },
 ];
 
 const splitItemsIntoRows = (items: CategoryItem[], itemsPerRow: number) => {
@@ -34,42 +35,49 @@ const splitItemsIntoRows = (items: CategoryItem[], itemsPerRow: number) => {
 
 const rows = splitItemsIntoRows(items, 4);
 
-interface Product {
-    id: string;
+type Product = {
+    id: number;
     name: string;
     price: string;
     image: string;
     description: string;
-}
-const products = [
-    { id: '1', name: 'Producto 1', price: '$10', image: 'https://via.placeholder.com/150', description: 'Descripción del Producto 1' },
-    { id: '2', name: 'Producto 2', price: '$15', image: 'https://via.placeholder.com/150',description: 'Descripción del Producto 2' },
-    { id: '3', name: 'Producto 3', price: '$10', image: 'https://via.placeholder.com/150',description: 'Descripción del Producto 3' },
-    { id: '4', name: 'Producto 4', price: '$15', image: 'https://via.placeholder.com/150',description: 'Descripción del Producto 4' },
-    { id: '5', name: 'Producto 5', price: '$10', image: 'https://via.placeholder.com/150',description: 'Descripción del Producto 5' },
-    { id: '6', name: 'Producto 6', price: '$15', image: 'https://via.placeholder.com/150',description: 'Descripción del Producto 6' },
-    // Agrega más productos aquí
-];
+    };
+
 
 
 const IndexCliente: React.FC = () => {
-    const [modalVisible, setModalVisible] = useState(false);
     const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const products: Product[] = [
+        { id: 1, name: 'Producto 1', price: '$10.00', image: 'https://via.placeholder.com/150', description: 'Descripción del Producto 1' },
+        { id: 2, name: 'Producto 2', price: '$15.00', image: 'https://via.placeholder.com/150', description: 'Descripción del Producto 2' },
+        { id: 3, name: 'Producto 3', price: '$12.00', image: 'https://via.placeholder.com/150', description: 'Descripción del Producto 3' },
+        { id: 4, name: 'Producto 4', price: '$18.00', image: 'https://via.placeholder.com/150', description: 'Descripción del Producto 4' },
+        // Agrega más productos aquí
+    ];
+    
+    
+    const handleProductPress = (product: Product) => {
+        setSelectedProduct(product);
+    };
 
     const handleOpenModal = (product: Product) => {
         setSelectedProduct(product);
         setModalVisible(true);
     };
 
-    const handleCloseModal = () => {
-        setModalVisible(false);
+    
+    const closeModal = () => {
         setSelectedProduct(null);
     };
 
 return (
-<ScrollView style={styles.container}>
+    <View style={styles.container}>
+        <NavBusqueda/>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
 
-    <NavBusqueda/>
+   
     
 {/* Carrusel de imágenes */}
     <ScrollView horizontal pagingEnabled style={styles.carouselContainer}>
@@ -78,9 +86,15 @@ return (
         <Image source={require('../../assets/images/carrucel3.jpg')} style={styles.carouselImage}resizeMode="cover"/>
     </ScrollView>
 
+
     {/* Sección de categorías */}
     <View style={styles.section}>
-                <Text style={styles.sectionTitle}>Categorías de Dulces</Text>
+        
+    <View style={{flexDirection:'row'}}>
+        <View style={styles.marker} /> 
+        <Text style={styles.sectionTitle}>Categorías de Dulces</Text>
+    </View>
+            
                 {rows.map((row, index) => (
                     <View key={index} style={styles.categoriesRow}>
                         {row.map(item => (
@@ -94,71 +108,66 @@ return (
     </View>
 
     {/* Sección de productos */}
-    <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Productos</Text>
-        <ScrollView>
-            <View style={styles.productsContainer}>
-                {products.map(product => (
-                    <TouchableOpacity 
-                        key={product.id} 
-                        style={styles.productItem} 
-                        onPress={() => handleOpenModal(product)}
-                    >
-                        <Image source={{ uri: product.image }} style={styles.productImage} />
-                        <Text style={styles.productText}>{product.name}</Text>
-                        <Text style={styles.productPrice}>{product.price}</Text>
-                    </TouchableOpacity>
-                ))}
-            </View>
-        </ScrollView>
-    </View>
+    <Text style={styles.sectionTitle2}>Productos</Text>
+    <View style={styles.productRow}>
+        {products.map((product: Product) => (
+            <TouchableOpacity key={product.id} style={styles.productCard} onPress={() => handleProductPress(product)}>
+            <Image source={{ uri: product.image }} style={styles.productImage} />
+            <Text style={styles.productName}>{product.name}</Text>
+            <Text style={styles.productPrice}>{product.price}</Text>
+            </TouchableOpacity>
+        ))}
+        </View>
 
-    {/* Modal del producto */}
-    <Modal
-        visible={modalVisible}
-        transparent
-        animationType="slide"
-        onRequestClose={handleCloseModal}
-    >
+    {/* Modal para mostrar detalles del producto */}
+    <Modal visible={!!selectedProduct} transparent={true} animationType="slide">
         <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-                <TouchableOpacity style={styles.closeButton} onPress={handleCloseModal}>
-                    <Ionicons name="close" size={30} color="black" />
+        <View style={styles.modalContent}>
+            {/* Botón de cierre con ícono */}
+            <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+            <Ionicons name="close" size={24} color="#000" />
+            </TouchableOpacity>
+            {selectedProduct && (
+            <>
+                <Text style={styles.productName}>{selectedProduct.name}</Text>
+                <Image source={{ uri: selectedProduct.image }} style={styles.modalProductImage} />
+                <Text style={styles.productDescription}>{selectedProduct.description}</Text>
+                <Text style={styles.productPrice}>{selectedProduct.price}</Text>
+                <TouchableOpacity style={styles.addButton}>
+                    <Text style={styles.addButtonText}>Añadir al carrito</Text>
+                    <FontAwesome name="shopping-cart" size={30} color="#FFF" />
                 </TouchableOpacity>
-                {selectedProduct && (
-                    <>
-                        <Text style={styles.modalTitle}>{selectedProduct.name}</Text>
-                        <Image source={{ uri: selectedProduct.image }} style={styles.modalImage} />
-                        <Text style={styles.modalPrice}>{selectedProduct.price}</Text>
-                        <Text style={styles.productDescription}>{selectedProduct.description}</Text>
-                        <TouchableOpacity style={styles.addToCartButton}>
-                            <Text style={styles.addToCartText}>Añadir al Carrito</Text>
-                        </TouchableOpacity>
-                    </>
-                )}
-            </View>
+            </>
+            )}
+        </View>
         </View>
     </Modal>
 </ScrollView>
+</View>
 );
 };
 
 const styles = StyleSheet.create({
 container: {
     flex: 1,
-    backgroundColor: '#FFF',
+    backgroundColor: '#f6f6f6',
+},
+scrollContainer: {
+    paddingTop: 70, // Espacio para el NavBusqueda
 },
 carouselContainer: {
-    height: 230,
+    marginTop:30,
+    height: 190,
     backgroundColor:'rgb(255, 211, 211)',
 },
 carouselImage: {
     width: screenWidth,
-    height: 230,
+    height: 220,
     borderRadius: 10,
 },
 section: {
-    marginTop: 20,
+    marginTop: 10,
+    marginBottom:5,
 },
 sectionTitle: {
     fontSize: 24,
@@ -166,7 +175,26 @@ sectionTitle: {
     color: '#333',
     textAlign: 'center',
     fontFamily:'Josefbold',
-
+    backgroundColor:'rgb(255, 255, 255)',
+    borderColor:'rgb(113, 113, 113)',
+},
+marker: {
+    width: 50,
+    height: 35,
+    backgroundColor: '#1caca8',
+    position: 'relative',
+    marginRight:10,
+    marginVertical:9,
+},
+sectionTitle2: {
+    fontSize: 24,
+    marginBottom: 15,
+    color: '#333',
+    textAlign: 'center',
+    fontFamily:'Josefbold',
+    backgroundColor:'rgba(221, 189, 222, 0.69)',
+    borderBottomWidth:9,
+    borderColor:'rgba(214, 128, 215, 0.5)',
 },
 categoriesContainer: {
     
@@ -200,7 +228,7 @@ categoryImage: {
     borderRadius:20,
 },
 categoryText: {
-    fontSize: 12,
+    fontSize: 13,
     color: '#333',
     fontFamily:'Josefinmedium',
 },
@@ -212,28 +240,35 @@ productsContainer: {
     backgroundColor:'rgb(255, 255, 255)',
 
 },
-productItem: {
-    width: 170,
-    alignItems: 'center',
-    marginLeft:20,
-    backgroundColor: 'rgb(255, 186, 186)',
-    borderRadius: 10,
+productRow: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+},
+productCard: {
+    width: '40%',
+    marginVertical: 16,
     padding: 10,
-    marginBottom:45,
+    borderRadius: 10,
+    alignItems: 'center',
+    elevation: 5,    
+    backgroundColor: '#ffffff',
+    borderColor: 'gray',
 },
 productImage: {
     width: 100,
-    height: 100,
-    borderRadius: 10,
-    marginBottom: 10,
+    height: 90,
+    borderRadius: 8,
 },
-productText: {
-    fontSize: 16,
-    color: '#333',
+productName: {
+    fontSize: 15,
+    marginVertical: 5,
+    fontFamily:'Josefinmedium',
 },
 productPrice: {
-    fontSize: 14,
-    color: '#888',
+    fontSize: 17,
+    color: '#106680',
+    fontFamily:'Josefbold',
 },
 modalContainer: {
     flex: 1,
@@ -243,10 +278,11 @@ modalContainer: {
 },
 modalContent: {
     width: '80%',
-    backgroundColor: '#FFF',
-    borderRadius: 10,
+    backgroundColor: '#fff',
     padding: 20,
+    borderRadius: 8,
     alignItems: 'center',
+    position: 'relative',
 },
 closeButton: {
     position: 'absolute',
@@ -255,36 +291,32 @@ closeButton: {
     padding:10,
     backgroundColor:'rgba(231, 124, 255, 0.49)',
 },
-modalImage: {
+modalProductImage: {
     width: 150,
     height: 150,
-    borderRadius: 10,
+    borderRadius: 8,
     marginBottom: 10,
-},
-modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-},
-modalPrice: {
-    fontSize: 18,
-    color: '#888',
-    marginBottom: 20,
-},
-addToCartButton: {
-    backgroundColor: '#4eacba',
-    padding: 10,
-    borderRadius: 5,
-    paddingHorizontal:60,
-},
-addToCartText: {
-    color: '#FFF',
-    fontSize: 16,
 },
 productDescription: {
     marginVertical: 10,
     fontSize: 14,
+    fontFamily:'Josefinreg',
     textAlign: 'center',
+},
+addButton: {
+    flexDirection: 'row',
+    marginTop: 10,
+    paddingVertical: 10,
+    borderRadius: 5,
+    backgroundColor: '#4eacba',
+    padding: 10,
+    paddingHorizontal:30,
+},
+addButtonText: {
+    fontSize:16,
+    color: '#fff',
+    fontFamily:'Josefinreg',
+    marginRight:20,
 },
 });
 
--- components/navBusqueda.tsx
+++ components/navBusqueda.tsx
@@ -1,143 +1,173 @@
-    import React, { useRef } from 'react';
-    import { View, Text, TextInput, TouchableOpacity, StyleSheet, DrawerLayoutAndroid } from 'react-native';
-    import { FontAwesome } from '@expo/vector-icons';
-    import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Modal, Pressable, Dimensions } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-    const NavBusqueda = () => {
-    // Define el tipo de referencia
-    const drawer = useRef<DrawerLayoutAndroid>(null);
+const { width: screenWidth } = Dimensions.get('window');
 
-    const navigationView = () => (
-        <View style={styles.drawerContainer}>
-            <View style={{height:160, backgroundColor:'rgb(0, 0, 0)'}}></View>
-                <Text style={styles.drawerItem}>Inicio</Text>
-                <Text style={styles.drawerItem}>Datos de usuario</Text>
-                <Text style={styles.drawerItem}>Carrito</Text>
-        </View>
-    );
+const NavBusqueda = () => {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const handleOpenModal = () => setModalVisible(true);
+    const handleCloseModal = () => setModalVisible(false);
 
     return (
-        <DrawerLayoutAndroid
-        ref={drawer}
-        drawerWidth={300}
-        drawerPosition="left"
-        renderNavigationView={navigationView}
-        >
+        <View style={styles.container}>
+            <View style={styles.navbar}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.navbarTitle}>POLAINA'S CANDYS</Text>
+                </View>
 
-        <View style={styles.navbar}>
-            <View style={styles.titleContainer}>
-            <Text style={styles.navbarTitle}>POLAINA'S CANDYS</Text>
-            </View>
+                <View style={styles.menuContainer}>
+                    <TouchableOpacity onPress={handleOpenModal}>
+                        <FontAwesome name="bars" size={24} color="#FFF" style={styles.menuButton} />
+                    </TouchableOpacity>
+                    
+                    <View style={styles.searchContainer}>
+                        <Ionicons name="search-outline" size={24} color="#666" style={styles.searchIcon} />
+                        <TextInput
+                            style={styles.searchInput}
+                            placeholder="Buscar dulces"
+                            placeholderTextColor="#666"
+                        />
+                    </View>
 
-            <View style={styles.menuContainer}>
-            <TouchableOpacity onPress={() => drawer.current?.openDrawer()}>
-                <FontAwesome name="bars" size={24} color="#FFF" style={styles.menuButton} />
-            </TouchableOpacity>
-            
-            <View style={styles.searchContainer}>
-                <Ionicons name="search-outline" size={24} color="#666" style={styles.searchIcon} />
-                <TextInput
-                style={styles.searchInput}
-                placeholder="Buscar dulces"
-                placeholderTextColor="#666"
-                />
-            </View>
-
-            <View style={styles.cartContainer}>
-                <FontAwesome name="shopping-cart" size={30} color="#FFF" />
-                <View style={styles.cartBadge}>
-                <Text style={styles.cartBadgeText}>1</Text>
+                    <View style={styles.cartContainer}>
+                        <FontAwesome name="shopping-cart" size={30} color="#FFF" />
+                        <View style={styles.cartBadge}>
+                            <Text style={styles.cartBadgeText}>1</Text>
+                        </View>
+                    </View>
                 </View>
             </View>
-            </View>
-        </View>
-        </DrawerLayoutAndroid>
-    );
-    };
 
+            {/* Modal del menú */}
+            <Modal
+                visible={modalVisible}
+                transparent
+                animationType="none"
+                onRequestClose={handleCloseModal}
+            >
+                <View style={styles.modalOverlay}>
+                    <View style={styles.modalContainer}>
+                        <Pressable style={styles.closeButton} onPress={handleCloseModal}>
+                            <Ionicons name="close" size={24} color="#FFF" />
+                        </Pressable>
+                        <Text style={styles.drawerItem}>Inicio</Text>
+                        <Text style={styles.drawerItem}>Datos de usuario</Text>
+                        <Text style={styles.drawerItem}>Carrito</Text>
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-navbar: {
-    marginTop:30,
-    backgroundColor: '#000000',
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#1D1D1D',
-},
-titleContainer: {
-    alignItems: 'center',
-    marginBottom: 15,
-},
-navbarTitle: {
-    fontSize: 22,
-    color: '#ffffff',
-    fontFamily: 'Lailasemi',
-},
-menuContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-},
-menuButton: {
-    marginRight: 10,
-},
-searchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-    borderRadius: 20,
-    flex: 1,
-    marginHorizontal: 15,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: '#CCC',
-},
-searchInput: {
-    backgroundColor: '#FFF',
-    borderRadius: 20,
-    paddingHorizontal: 20,
-    paddingVertical: 5,
-    color: '#333',
-    flex: 1,
-    fontFamily: 'Josefinli',
-    fontSize:17,
-},
-searchIcon: {
-    marginLeft: 10,
-},
-cartContainer: {
-    position: 'relative',
-},
-cartBadge: {
-    position: 'absolute',
-    right: -10,
-    top: -10,
-    backgroundColor: '#65b196',
-    borderRadius: 10,
-    width: 20,
-    height: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-},
-cartBadgeText: {
-    color: 'white',
-    fontSize: 12,
-    fontWeight: 'bold',
-},
-drawerContainer: {
-    flex: 1,
-    backgroundColor: '#ffffff',  
-},
-drawerItem: {
-    fontSize: 18,
-    paddingLeft:30,
-    paddingVertical:10,
-    fontFamily: 'Laila',
-},
-headermenu:{
-    backgroundColor:'rgb(0, 0, 0)',
-}
-
+    container: {
+        flex: 1,
+        marginTop:25,
+    },
+    navbar: {
+        backgroundColor: 'rgb(39, 39, 39)',
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#1D1D1D',
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        zIndex: 1000, // Asegúrate de que esté sobre otros contenidos
+    },
+    titleContainer: {
+        alignItems: 'center',
+        marginBottom: 15,
+    },
+    navbarTitle: {
+        fontSize: 22,
+        color: '#ffffff',
+        fontFamily: 'Lailasemi',
+    },
+    menuContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    menuButton: {
+        marginRight: 10,
+    },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        borderRadius: 20,
+        flex: 1,
+        marginHorizontal: 15,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: '#CCC',
+    },
+    searchInput: {
+        backgroundColor: '#FFF',
+        borderRadius: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        color: '#333',
+        flex: 1,
+        fontFamily: 'Josefinli',
+        fontSize: 17,
+    },
+    searchIcon: {
+        marginLeft: 10,
+    },
+    cartContainer: {
+        position: 'relative',
+    },
+    cartBadge: {
+        position: 'absolute',
+        right: -10,
+        top: -10,
+        backgroundColor: '#65b196',
+        borderRadius: 10,
+        width: 20,
+        height: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cartBadgeText: {
+        color: 'white',
+        fontSize: 12,
+        fontWeight: 'bold',
+    },
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end', // Alinea al modal a la derecha
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContainer: {
+        width: 250,
+        height:'100%',
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'flex-start',
+        position: 'absolute',
+        left: 0, // Alinea el modal al borde derecho
+        top: 0, // Alinea el modal al borde superior
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        backgroundColor: '#000',
+        borderRadius: 20,
+        padding: 10,
+    },
+    drawerItem: {
+        fontSize: 18,
+        paddingVertical: 10,
+        fontFamily: 'Laila',
+    },
 });
 
 export default NavBusqueda;
